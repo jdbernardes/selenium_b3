cbab2ac89c73d09e335609ab16edfc5c17f3bb1e
jdbernardes <juliocd.bernardes@gmail.com>
2024-06-28 16:24:40 -0300

    renaming DB columns since it was causing errors on Glue

--- selenium_b3/upload.py
+++ selenium_b3/upload.py
@@ -11,7 +11,7 @@ from datetime import datetime
 date = datetime.now().strftime('%d-%m-%y')
 csv_file_name = f'IBOVDia_{date}.csv'
 
-bucket_name = "fiap-brisa-general"
+bucket_name = "fiap-julio-general"
 bucket_layer = f"raw/ibovespa/{date}"
 
 input_path_csv = f'data/{csv_file_name}'
@@ -20,13 +20,14 @@ output_path_parquet = f'data/IBOVDia_{date}.parquet'
 #%%
 #Convert local csv file to parquet
 def csv_to_parquet(input_path_csv, output_path_parquet):
-    df = pd.read_csv(input_path_csv, encoding='latin-1', sep = ';', skiprows=1, skipfooter=2, index_col=False, engine='python', dtype=str)
-    df['Qtde. Teórica'] = df['Qtde. Teórica'].str.replace(".","")
-    df['Part. (%)'] = df['Part. (%)'].str.replace(',','.')
-    df['Part. (%)Acum.'] = df['Part. (%)Acum.'].str.replace(',','.')
-    df['Qtde. Teórica'] = df['Qtde. Teórica'].astype(int)
-    df['Part. (%)'] = df['Part. (%)'].astype(float)
-    df['Part. (%)Acum.'] = df['Part. (%)Acum.'].astype(float)
+    colnames=['setor', 'codigo', 'acao', 'tipo', 'qtde_teorica', 'part_percent', 'part_percent_acum']
+    df = pd.read_csv(input_path_csv,names=colnames, encoding='latin-1', sep = ';', skiprows=2, skipfooter=2, index_col=False, engine='python', dtype=str)
+    df['qtde_teorica'] = df['qtde_teorica'].str.replace(".","")
+    df['part_percent'] = df['part_percent'].str.replace(',','.')
+    df['part_percent_acum'] = df['part_percent_acum'].str.replace(',','.')
+    df['qtde_teorica'] = df['qtde_teorica'].astype(int)
+    df['part_percent'] = df['part_percent'].astype(float)
+    df['part_percent_acum'] = df['part_percent_acum'].astype(float)
     table = pa.Table.from_pandas(df)
     pq.write_table(table, output_path_parquet)
 
