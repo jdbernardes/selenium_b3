a2233c169f7b42f6474ecef84b8bb122678ab2d1
jdbernardes <juliocd.bernardes@gmail.com>
2024-07-04 21:37:40 -0300

    bug fix - bucket path was wrong

--- selenium_b3/main.py
+++ selenium_b3/main.py
@@ -17,11 +17,10 @@ csv_file_name = f'IBOVDia_{date_file}.csv'
 input_path_csv = f'data/{csv_file_name}'
 output_path_parquet = f'data/IBOVDia_{trading_date}.parquet'
 parquet_file_name = f'IBOVDia_{trading_date}.parquet'
-parquet_file_name = f'./data/{parquet_file_name}'
+parquet_file_path = f'./data/{parquet_file_name}'
 bucket_layer = f'raw/ibovespa/{trading_date}'
 object_name = f'{bucket_layer}/{parquet_file_name}'
-
 data_transform.save_parquet(input_path_csv, output_path_parquet)
 
 aws = UploadAws()
-aws.upload_file(parquet_file_name, bucket_name, object_name)
+aws.upload_file(parquet_file_path, bucket_name, object_name)
